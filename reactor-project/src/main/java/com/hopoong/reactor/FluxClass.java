9ce1b08a71016197f4f70424cfd5bb3737021b02
ho_poong <hopoong23@gmail.com>
2024-07-25 21:01:40 +0900

    #fix - mono add.

--- reactor-project/src/main/java/com/hopoong/reactor/FluxClass.java
+++ reactor-project/src/main/java/com/hopoong/reactor/FluxClass.java
@@ -2,10 +2,14 @@ package com.hopoong.reactor;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
+
 public class FluxClass {
 
 
-    public static void main(String[] args) {
+    // Flux는 0 또는 다수의 요소를 포함하는 리액티브 타입
+    public static void main(String[] args) throws InterruptedException {
 
         // 기본 Flux 생성 및 구독
         Flux<String> fluxStr = Flux.just("String", "String Boot", "Reactor", "String WebFlux");
@@ -44,6 +48,28 @@ public class FluxClass {
         Flux<String> concatFlux = Flux.concat(fluxObj1, fluxObj2);
         concatFlux.subscribe(System.out::println);
         System.out.println("================================= 6");
+
+        // zip
+        Flux<String> zippedFlux = Flux.zip(fluxObj1, fluxObj2, (color, shpape) -> color + " :: " + shpape);
+        zippedFlux.subscribe(System.out::println);
+        System.out.println("================================= 7");
+
+
+        // Error
+        Flux<Integer> fluxa = Flux.range(1, 10)
+                .map(val -> {
+                    if(val == 5)
+                        throw new RuntimeException("Error at 5");
+                    return val;
+                }).onErrorResume(e -> Flux.just(-1, -2));
+
+        fluxa.subscribe(val -> System.out.println(val));
+        System.out.println("================================= 8");
+
+        // 비동기 작업 처리
+        Flux<Long> fluxInterval = Flux.interval(Duration.ofMillis(500)).take(7);
+        fluxInterval.subscribe(System.out::println);
+        Thread.sleep(1000);
     }
 
 
---
+++ reactor-project/src/main/java/com/hopoong/reactor/MonoClass.java
@@ -0,0 +1,9 @@
+package com.hopoong.reactor;
+
+public class MonoClass {
+
+    // Mono는 0 또는 1개의 요소를 포함하는 리액티브 타입
+    public static void main(String[] args) {
+
+    }
+}
