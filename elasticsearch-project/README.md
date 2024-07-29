ca2fd9dc830a258469d86643d6951bdaf85abd17
ho_poong <hopoong23@gmail.com>
2024-07-29 18:47:03 +0900

    #fix - elasticsearch-project 구상.

---
+++ elasticsearch-project/README.md
@@ -0,0 +1,6 @@
+## Referenced Sites
+
+- [Kim Jmin's Elasticsearch Book](https://esbook.kimjmin.net/)
+- [FastCampus Elasticsearch Course](https://fastcampus.co.kr/pages/29573)
+- [Elasticsearch Korean Spell Suggest Project](https://github.com/nobaksan/Elasticsearch-Korean-Spell-Suggest)
+- [ElasticSearch Related Blog Posts](https://oyoungsun.tistory.com/category/ElasticSearch)
--- reactor-project/src/main/java/com/hopoong/reactor/MonoClass.java
+++ reactor-project/src/main/java/com/hopoong/reactor/MonoClass.java
@@ -1,9 +1,26 @@
 package com.hopoong.reactor;
 
+import reactor.core.publisher.Mono;
+
 public class MonoClass {
 
     // Mono는 0 또는 1개의 요소를 포함하는 리액티브 타입
     public static void main(String[] args) {
+
+        Mono<String> strMono = Mono.just("Mono");
+        strMono.subscribe(System.out::println);
+        System.out.println("================================= 1");
+
+
+        Mono<String> emptStr = Mono.error(new Exception("Error"));
+        emptStr.subscribe(
+                System.out::println,
+                error -> System.out.println("Error"),
+                () -> System.out.println("Completed")
+        );
+        System.out.println("================================= 1");
+
+
 
     }
 }
